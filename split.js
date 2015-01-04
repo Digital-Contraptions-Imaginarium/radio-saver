22ba99ace0c6122b0d41a9592a206aa367f15d36
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2015-01-04 08:47:08 +0000

    Opening the file in Atom did something strange

--- split.js
+++ split.js
@@ -20,7 +20,7 @@ var async = require('async'),
 var toBeIgnored = !argv.ignore ? [ ] : fs
 	.readFileSync(path.join(argv.out, 'ignore.txt'), { 'encoding': 'utf8' })
 	.split('\n')
-	.reduce(function (memo, line) { 
+	.reduce(function (memo, line) {
 		line = line.trim();
 		if ((line !== '') && !line.match(/^#/)) memo = memo.concat(line);
 		return memo;
@@ -29,9 +29,9 @@ var toBeIgnored = !argv.ignore ? [ ] : fs
 var tracks = fs
 	.readFileSync(argv.in + '.jsonl', { 'encoding': 'utf8' })
 	.split('\n')
-	.reduce(function (memo, line) { 
+	.reduce(function (memo, line) {
 		if (line.trim() !== '') memo = memo.concat(JSON.parse(line));
-		return memo; 
+		return memo;
 	}, [ ]);
 // calculates the tracks' duration
 for (var i = tracks.length - 2; i > 0; i--) {
@@ -43,11 +43,12 @@ tracks = _.rest(_.initial(tracks));
 tracks = tracks.filter(function (track) { return track.artist || track.title; });
 // does the trimming
 async.eachSeries(
-	tracks, 
+	tracks,
 	function (track, callback) {
 		var basename = track.artist + ' - ' + track.title;
 		if (!_.contains(toBeIgnored, basename)) {
 			tentativeFilename.tryFilename(path.join(argv.out, basename), 'mp3', function (err, filename) {
+				// TODO: if the specified trimming duration goes beyond the actual duration of the file, sox will return a warning apparently the trimming works anyway
 				var command = SOX_PATH + ' "' + argv.in + '" "' + filename + '" trim ' + Math.max(0, track.timestamp - parseInt(argv.left)) + ' ' + (track.duration + parseInt(argv.left) + parseInt(argv.right));
 				console.log(command);
 				exec(
@@ -74,5 +75,3 @@ async.eachSeries(
 
 	}
 );
-
-
